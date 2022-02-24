c8dbabec99785ac7e1bc10a8f690e79597a8bdec
Fadil Bakre <fadilbaqrey@gmail.com>
2022-02-24 13:47:50 +0100

    Initial commit

---
+++ app/src/main/java/com/fact/hiandroiddevelopers/MainActivity.java
@@ -0,0 +1,18 @@
+package com.fact.hiandroiddevelopers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+class Car{
+
+}
